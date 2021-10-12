84f6babb07e00c3ec88157585f3d3e6eafa1f680
Jefferson Medina <medina_pein@hotmail.com>
2021-10-12 12:26:46 -0500

    Configuracion de los Contenedores de Nodejs y MySQL

--- myweb_node/index.js
+++ myweb_node/index.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const express = require("express");
-const PORT = 3000;
+require("dotenv").config();
+
+const express = require('express');
+const PORT = process.env.NODE_DOCKER_PORT || 3000;
 const HOST = '0.0.0.0';
 
+// App
 const app = express();
-
 app.get('/', (req, res) => {
-    res.send("Hello World!!");
+    res.send('Hello World!!!');
 });
 
 app.listen(PORT, HOST);
-
 console.log(`Running on http://${HOST}:${PORT}`);
