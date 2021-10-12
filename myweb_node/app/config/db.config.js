95dfed521e9f8b4b78a28f1966ff786bb4bb2d66
Jefferson Medina <medina_pein@hotmail.com>
2021-10-12 18:23:54 -0500

    Estructura del proeycto

--- myweb_node/app/config/db.config.js
+++ myweb_node/app/config/db.config.js
@@ -1,14 +1,12 @@
-module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
+const { createPool } = require("mysql");
+
+const db = createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-};
+    connectionLimit: 10
+});
+
+module.exports = db;
